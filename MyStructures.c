eaee2891bd530c863cb8be02bfab92913a71b895
Mikhail Kiselev <muxa.kisel.05@gmail.com>
2024-09-27 17:48:14 +0700

    Добавлено еще 3 структуры данных, с функциями для их проверки

--- MyStructures.c
+++ MyStructures.c
@@ -5,4 +5,43 @@ int FinalDMG(int PersonalDMG, int EnemyDFN) {
     if (PersonalDMG - EnemyDFN < 0)
         return 0;
     return PersonalDMG - EnemyDFN;
-};
+}
+
+int AllDefence(int HelmDef, int CuirDef, int GlovDef, int GreaDef, int BootDef) {
+    return HelmDef + CuirDef + GlovDef + GreaDef + BootDef;
+}
+
+void CreateBiome(char *Name, int *RecomendLevel, int *MapSize) {
+    scanf("%s %d %d", Name, RecomendLevel, MapSize);
+}
+
+void CheckAttributes(int Strength, int Intelligence, int Agility) {
+    printf("Ваш персонаж: ");
+
+    if (Strength > 0 && Strength <= 25)
+        printf("Хрупкий, ");
+    else if (Strength > 25 && Strength <= 50)
+        printf("Слабый, ");
+    else if (Strength > 50 && Strength <= 75)
+        printf("Крепкий, ");
+    else if (Strength > 75 && Strength <= 100)
+        printf("Могучий, ");
+
+    if (Intelligence > 0 && Intelligence <= 25)
+        printf("Слабоумный, ");
+    else if (Intelligence > 25 && Intelligence <= 50)
+        printf("Глупый, ");
+    else if (Intelligence > 50 && Intelligence <= 75)
+        printf("Знающий, ");
+    else if (Intelligence > 75 && Intelligence <= 100)
+        printf("Умный, ");
+
+    if (Agility > 0 && Agility <= 25)
+        printf("Неуклюжий.\n ");
+    else if (Agility > 25 && Agility <= 50)
+        printf("Неловкий.\n");
+    else if (Agility > 50 && Agility <= 75)
+        printf("Ловкий.\n");
+    else if (Agility > 75 && Agility <= 100)
+        printf("Изящный.\n");
+}
--- MyStructures.h
+++ MyStructures.h
@@ -1,14 +1,45 @@
 #ifndef MYSTRUCTURES_H_INCLUDED
 #define MYSTRUCTURES_H_INCLUDED
 
-//Хранит в себе урон нападающего и защиту обороняющегося
-typedef struct
-{
+//№1 Хранит в себе урон нападающего и защиту обороняющегося
+typedef struct {
     int PersonalDMG;
     int EnemyDFN;
 }AttackDMG;
 
-//Функция для проверки предыдущей структуры
+//Функция для проверки структуры №1
 int FinalDMG(int PersonalDMG, int EnemyDFN);
+
+//№2 Хранит в себе общий показатель брони
+typedef struct {
+    int HelmetDefence;
+    int CuirassDefence;
+    int GlovesDefence;
+    int GreavesDefence;
+    int BootsDefence;
+}Defence;
+
+//Функция для проверки структуры №2
+int AllDefence(int HelmDef, int CuirDef, int GlovDef, int GreaDef, int BootDef);
+
+//№3 Содержит в себе информацию о биоме
+typedef struct {
+    char Name[10];
+    int RecomendLevel;
+    int MapSize;
+}Biome;
+
+//Функция для проверки структуры №3
+void CreateBiome(char *Name, int *RecomendLevel, int *MapSize);
+
+//№4 Содержит информацию об атрибутах
+typedef struct {
+    int Strength;
+    int Intelligence;
+    int Agility;
+}Attributes;
+
+//Функция для проверки структуры №4
+void CheckAttributes(int Strength, int Intelligence, int Agility);
 
 #endif // MYSTRUCTURES_H_INCLUDED
